de1131c86312da469216c23a37ca5880b3140b9f
Michal Gosek <inbox@michalgosek.com>
2021-06-07 19:05:21 +0200

    init commit.

---
+++ src/main/java/com/example/vidley/VidleyApplication.java
@@ -0,0 +1,13 @@
+package com.example.vidley;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VidleyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VidleyApplication.class, args);
+	}
+
+}
